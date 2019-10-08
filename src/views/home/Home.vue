47618a37e8861fbbaa25165c2fbec812e5c3a138
HYP-hu <fluentpython@163.com>
2019-10-08 08:54:55 +0800

    初始化文件

---
+++ src/views/home/Home.vue
@@ -0,0 +1,98 @@
+<template>
+<div id="home">
+  <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+  <div>1111</div>
+</div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar";
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    }
+  }
+</script>
+
+<style scoped>
+  #home{
+    /*height: 100vh;*/
+    padding-top: 44px;
+    position: relative;
+  }
+
+  .home-nav{
+    background-color: var(--color-tint);
+    color: #fff;
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9
+  }
+</style>
