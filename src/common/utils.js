fe0a4b9811a832c7eb1592caa0a7ddc44e221bd5
HYP-hu <fluentpython@163.com>
2019-10-10 19:10:37 +0800

    增加评论组件配置时间转换方法

--- src/common/utils.js
+++ src/common/utils.js
@@ -7,3 +7,33 @@ export function debounce(func, delay) {
     }, delay);
   };
 }
+
+// 如何将时间戳转成时间格式化字符串(常用)
+// 时间戳: 1535694719
+// 1.将时间戳转成Date对象
+// const date = new Date(1535694719)
+// 2.将date格式化,转成对应的字符串
+// date.getYear() + date.getMonth() + 1
+export function formatDate(date, fmt) {
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+  }
+  let o = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  };
+  for (let k in o) {
+    if (new RegExp(`(${k})`).test(fmt)) {
+      let str = o[k] + '';
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+    }
+  }
+  return fmt;
+};
+
+function padLeftZero (str) {
+  return ('00' + str).substr(str.length);
+};
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -5,8 +5,9 @@
       <detail-swiper :top-images="topImages" />
       <detail-base-info :goods="goods" />
       <detail-shop-info :shop="shop" />
-      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
-      <detail-param-info :param-info="paramInfo"></detail-param-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad" />
+      <detail-param-info :param-info="paramInfo" />
+      <detail-comment-info :comment-info="commentInfo" />
     </scroll>
   </div>
 </template>
@@ -17,7 +18,8 @@ import DetailSwiper from "./childComps/DetailSwiper";
 import DetailBaseInfo from "./childComps/DetailBaseInfo";
 import DetailShopInfo from "./childComps/DetailShopInfo";
 import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
-import DetailParamInfo from "./childComps/DetailParamInfo"
+import DetailParamInfo from "./childComps/DetailParamInfo";
+import DetailCommentInfo from "./childComps/DetailCommentInfo";
 
 import Scroll from "components/common/scroll/Scroll";
 
@@ -36,7 +38,8 @@ export default {
       goods: {},
       shop: {},
       detailInfo: {},
-      paramInfo: {}
+      paramInfo: {},
+      commentInfo: {}
     };
   },
   components: {
@@ -46,13 +49,15 @@ export default {
     DetailShopInfo,
     Scroll,
     DetailGoodsInfo,
-    DetailParamInfo
+    DetailParamInfo,
+    DetailCommentInfo
   },
   created() {
     // 1. 保存传入的id
     // this.id = this.$route.params.id
     // 2.根据id请求详情数据
     getDeatil(this.id).then(res => {
+      // 获取的数据
       const data = res.result;
       // 1.获取顶部的图片轮播数据
       this.topImages = data.itemInfo.topImages;
@@ -67,12 +72,18 @@ export default {
       // 4.获取商品详细信息
       this.detailInfo = data.detailInfo;
       // 5.获取参数信息
-      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+      this.paramInfo = new GoodsParam(
+        data.itemParams.info,
+        data.itemParams.rule
+      );
+      // 6.保存评论数据
+      console.log(data);
+      this.commentInfo = data.rate.cRate !== 0 ? data.rate.list[0] : {};
     });
   },
   methods: {
-    imageLoad(){
-      this.$refs.scroll.refresh()
+    imageLoad() {
+      this.$refs.scroll.refresh();
     }
   }
 };
